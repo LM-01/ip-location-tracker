83fd46953d46236c66329d325ba7a84c29a8abf4
LM-01 <mollinedo.lester@gmail.com>
2021-06-25 10:00:14 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='main-container'>
+        <div className='content'>
+          <h1>IP Address Tracker</h1>
+          <div className='input-container'>
+            <input placeholder='Enter IP Value'></input>
+          </div>
+          <div className='info-container'>
+            <div className='info-container--row'>
+              <p className='small-heading'>Ip Address</p>
+              <p className='big-content'>Big Content</p>
+            </div>
+            <div className='info-container--row'>
+              <p className='small-heading'>Location</p>
+              <p className='big-content'>Big Content</p>
+            </div>
+            <div className='info-container--row'>
+              <p className='small-heading'>Timezone</p>
+              <p className='big-content'>Big Content</p>
+            </div>
+            <div className='info-container--row'>
+              <p className='small-heading'>ISP</p>
+              <p className='big-content'>Big Content</p>
+            </div>
+          </div>
+        </div>
+        <div className='background'>
+          stete
+        </div>
+        <div className='map'>
+tee
+        </div>
+
+      </div>
+    </>
   );
 }
 
